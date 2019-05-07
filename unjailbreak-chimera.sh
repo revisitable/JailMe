e08ed673a875a8b853a4853f1b137aff818a11d8
jay <38664452+sukarodo@users.noreply.github.com>
2019-05-07 17:18:47 -0500

    Create unjailbreak-chimera.sh

---
+++ unjailbreak-chimera.sh
@@ -0,0 +1,11 @@
+if [ "$EUID" -ne 0 ]; then
+  echo "ERROR! You are not logged in as root user!"
+  echo "Please login as root user before executing JailMe."
+  exit
+fi
+
+echo "JailMe by Sukarodo for Chimera"
+echo "This seperate version is for testing until the deicision of this being stable"
+echo "Once it is tested, this will be merged with JailMe as a whole"
+echo "Assuming you have not installed any other jailbreak or modified the rootfs directly yourself, you should be on stock iOS once this is complete."
+read -p "Press enter to continue. Press Ctrl + C to exit"
