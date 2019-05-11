cb69dc4c06c3d710251558c2035fde4b73658fa3
jay <38664452+sukarodo@users.noreply.github.com>
2019-05-11 17:00:24 -0500

    Kill Sileo & Cydia

--- unjailbreak-chimera.sh
+++ unjailbreak-chimera.sh
@@ -9,3 +9,9 @@ echo "This seperate version is for testing until the deicision of this being sta
 echo "Once it is tested, this will be merged with JailMe as a whole"
 echo "Assuming you have not installed any other jailbreak or modified the rootfs directly yourself, you should be on stock iOS once this is complete."
 read -p "Press enter to continue. Press Ctrl + C to exit"
+
+echo "Killing Sileo..."
+killall Sileo
+
+echo "Killing Cydia... (If Diatrus' Version Is Installed)"
+killall Cydia
